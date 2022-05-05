bb4ead2103f5c40d1e1cae75b7dd7d6479cd8e67
guilherme.garcia@ahgora.com.br <rgui.garcia@gmail.com>
2022-05-05 19:07:55 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Metaweather
+A Android app built with Jetpack Compose using kotlin on Metaweather API
---
+++ app/src/main/java/com/mega/metaweather/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.mega.metaweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mega.metaweather.ui.theme.MetaweatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MetaweatherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MetaweatherTheme {
+        Greeting("Android")
+    }
+}
