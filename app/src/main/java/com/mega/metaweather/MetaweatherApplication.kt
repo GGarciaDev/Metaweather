649ab05eb09d97d9f9f2ffd01b0505e81ec301b7
guilherme.garcia@ahgora.com.br <rgui.garcia@gmail.com>
2022-05-05 22:10:13 -0300

    feat: update gradle and start hilt app

---
+++ app/src/main/java/com/mega/metaweather/MetaweatherApplication.kt
@@ -0,0 +1,7 @@
+package com.mega.metaweather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MetaweatherApplication : Application()
--- app/src/main/java/com/mega/metaweather/presentation/MainActivity.kt
+++ app/src/main/java/com/mega/metaweather/presentation/MainActivity.kt
@@ -1,38 +1,23 @@
-package com.mega.metaweather
+package com.mega.metaweather.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.mega.metaweather.ui.theme.MetaweatherTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MetaweatherTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LocationScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MetaweatherTheme {
-        Greeting("Android")
     }
 }
